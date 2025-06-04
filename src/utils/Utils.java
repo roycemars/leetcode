73678458262a3e0788e37b8dbee3698b200a66bf
Royce Mars <roycemars@proton.me>
2025-06-03 20:13:22 -0500

    Code cleanup

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -2,10 +2,10 @@ package utils;
 
 public class Utils {
     public static void print(int[] r) {
-        System.out.printf("r = {");
+        System.out.print("r = {");
         for (int i = 0; i < r.length; i++) {
             System.out.printf("%d%s", r[i], i < r.length - 1 ? " " : "");
         }
-        System.out.printf("}\n");
+        System.out.print("}\n");
     }
 }
