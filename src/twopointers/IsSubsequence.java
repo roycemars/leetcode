880b827e361a7217af75b42e965b7f4ad6d9ad34
Royce Mars <roycemars@proton.me>
2025-06-03 20:27:58 -0500

    Two pointers: Is subsequence problem

---
+++ src/twopointers/IsSubsequence.java
@@ -0,0 +1,25 @@
+package twopointers;
+
+public class IsSubsequence {
+    public static void main(String[] args) {
+        String s = "ace", t = "abcde";
+
+        boolean r = isSubsequence(s, t);
+
+        System.out.printf("\"%s\" is subsequence of \"%s\": %b", s, t, r);
+    }
+
+    private static boolean isSubsequence(String s, String t) {
+        int i = 0, j = 0;
+
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
+            }
+            j++;
+        }
+
+        return i == s.length();
+    }
+
+}
