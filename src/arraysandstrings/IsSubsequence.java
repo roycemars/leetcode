7b4a131d4c0819fb23b4ee84be0d0f1f390b1c65
Royce Mars <roycemars@proton.me>
2025-06-06 11:33:26 -0500

    Refactoring: IsSubsequence javadoc updated

--- src/arraysandstrings/IsSubsequence.java
+++ src/arraysandstrings/IsSubsequence.java
@@ -1,5 +1,11 @@
 package arraysandstrings;
 
+/**
+ * Given two strings s and t, return true if s is a subsequence of t, or false otherwise.
+ * Example:
+ * For example, "ace" is a subsequence of "abcde" while "aec" is not.
+ */
+
 public class IsSubsequence {
     public static boolean isSubsequence(String s, String t) {
         int i = 0, j = 0;
