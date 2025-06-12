9bbfff6c16c4eef3bdc9d31bea04851af2009bea
Royce Mars <roycemars@proton.me>
2025-06-12 00:31:00 -0500

    Problem: Two Sum Sorted Approach: Two pointers

---
+++ src/linkedlists/TwoSumSorted.java
@@ -0,0 +1,22 @@
+package linkedlists;
+
+public class TwoSumSorted {
+    public static boolean checkForTarget(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int curr = nums[left] + nums[right];
+            if (curr == target) {
+                return true;
+            }
+
+            if (curr > target) {
+                right --;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+}
---
+++ test/linkedlists/TwoSumSortedTest.java
@@ -0,0 +1,19 @@
+package linkedlists;
+
+import org.junit.jupiter.api.Test;
+
+import static linkedlists.TwoSumSorted.checkForTarget;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumSortedTest {
+
+    @Test
+    void checkForTargetTestPositive() {
+        assertTrue(checkForTarget(new int[]{1, 2, 4, 6, 8, 9, 12, 15}, 9));
+    }
+
+    @Test
+    void checkForTargetTestNegative() {
+        assertFalse(checkForTarget(new int[]{1, 2, 4, 6, 9, 10, 12, 15}, 9));
+    }
+}
