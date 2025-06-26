cc949648f09781c746a8d4c8b902b2baada1a1eb
Royce Mars <roycemars@proton.me>
2025-06-26 08:42:12 -0500

    Refactoring: cleanup

--- src/test/kotlin/arraysandstrings/LengthOfLongestSubarrayTest.kt
+++ src/test/kotlin/arraysandstrings/LengthOfLongestSubarrayTest.kt
@@ -1,13 +1,12 @@
 package arraysandstrings
 
 import arraysandstrings.LengthOfLongestSubarray.Companion.findLength
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import kotlin.test.Test
 
-class LengthOfLongestSubarrayJavaKtTest {
-    @Test
-    fun findLengthTest() {
-        assertEquals(2, findLength(intArrayOf(3, 1, 2, 7, 4, 2, 1, 1, 5), 8))
-    }
-
+class LengthOfLongestSubarrayTest {
+  @Test
+  fun lengthOfLongestSubarrayTest() {
+      assertEquals(2, findLength(intArrayOf(3, 1, 2, 7, 4, 2, 1, 1, 5), 8))
+  }
 }
