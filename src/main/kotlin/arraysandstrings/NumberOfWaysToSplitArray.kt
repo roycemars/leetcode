d679b4e5cdaeaeb33aa966aabb048bc0ae050327
Royce Mars <roycemars@proton.me>
2025-06-29 15:14:27 -0500

    NumberOfWaysToSplitArray (prefix sum)

--- src/main/kotlin/arraysandstrings/NumberOfWaysToSplitArray.kt
+++ src/main/kotlin/arraysandstrings/NumberOfWaysToSplitArray.kt
@@ -5,6 +5,8 @@ class NumberOfWaysToSplitArray {
      * Given an integer array nums, find the number of ways to split the array into two parts so that
      * the first section has a sum greater than or equal to the sum of the second section.
      * The second section should have at least one number.
+     *
+     * Approach: prefix sum
      */
     fun splitArray(nums: IntArray): Int {
         val prefix = IntArray(nums.count())
