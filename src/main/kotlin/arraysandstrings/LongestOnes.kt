6e457ff8988a27d2fe274712713faf2b4a177426
Royce Mars <roycemars@proton.me>
2025-07-01 13:52:47 -0500

    LongestOnes

---
+++ src/main/kotlin/arraysandstrings/LongestOnes.kt
@@ -0,0 +1,25 @@
+package arraysandstrings
+
+import kotlin.math.max
+
+class LongestOnes {
+
+    /**
+     * Given a binary array nums and an integer k,
+     * return the maximum number of consecutive 1's in the array
+     * if you can flip at most k 0's.
+     */
+    fun longestOnes(nums: IntArray, k: Int): Int {
+        var (l, r) = 0 to 0
+        var zero = 0
+        var res = 0
+
+        while (r < nums.size) {
+            if (nums[r++] == 0) zero++
+            if (zero > k && nums[l++] == 0) zero--
+            res = max(res, r - l)
+        }
+
+        return res
+    }
+}
---
+++ src/test/kotlin/arraysandstrings/LongestOnesTest.kt
@@ -0,0 +1,16 @@
+package arraysandstrings
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+class LongestOnesTest {
+    @Test
+    fun longestOnesTwo() {
+        assertEquals(6, LongestOnes().longestOnes(intArrayOf(1,1,1,0,0,0,1,1,1,1,0), 2))
+    }
+
+    @Test
+    fun longestOnesThree() {
+        assertEquals(10, LongestOnes().longestOnes(intArrayOf(0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1), 3))
+    }
+}
